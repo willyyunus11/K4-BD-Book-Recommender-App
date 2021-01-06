02335e98822e666c60d05a3e8150914e1bf80bb0
daurelius <55678808+daurelius@users.noreply.github.com>
2021-01-06 19:17:45 +0700

    Add HTML CSS templates

--- book_recommendation/views.py
+++ book_recommendation/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+  return render(request, 'recommendation.html')
+  
+def firstRating(request):
+  return render(request, 'first_rating.html')
